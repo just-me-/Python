dba6f5704437a36048a4907b12904b1d9a3eb1a1
just-me- <hess-marcel@bluewin.ch>
2019-04-13 16:02:15 +0200

    Cleanup Flake Mimimi

--- 01_Testat/quizrangliste.py
+++ 01_Testat/quizrangliste.py
@@ -1,21 +1,29 @@
 # -*- coding: utf-8 -*-
 # quizrangliste.py
 
-# -----------------------------------------------------------------------------
+import re
+
+
 class QuizRangliste():
+    '''Schnittstelle zur Verwaltung der HSRvote-Software-Textdaten.'''
 
     def __extract_file_data(self, file):
         for zeile in file:
-            elemente = zeile.strip().split(',')
-            # array of array... or would be an array of object better? 2Do
-            self.__file_data.append(elemente)
-            # 2Do:
             # Leere oder ungültige Zeilen werden ignoriert.
+            if re.match(r'\w+,.*,.*$', zeile.strip()) is None:
+                continue
+            elemente = zeile.strip().split(',')
+            self.__file_data.append({
+                'name': elemente[0],
+                'punkte': int(elemente[1]),
+                'zeit': float(elemente[2])
+            })
 
     def __init__(self, datei='default.txt'):
-        '''Initialisiert der Rangliste.
-        List die Daten aus der angegebenen Textdatei (encoding='utf-8') aus
-        und bereitet diese zur Abfrage vor.
+        '''
+        Initialisierung der Rangliste.
+        List die Daten aus der angegebenen CSV-Textdatei (encoding='utf-8')
+        aus und bereitet diese zur Abfrage vor.
 
         Leere oder ungültige Zeilen werden ignoriert.
 
@@ -23,7 +31,7 @@ class QuizRangliste():
         die angegebene Datei noch nicht existiert.
 
         Argumente:
-            type(datei) == str -- Pfad zur Textdatei mit den Ranglistendaten.
+            datei: string -- Pfad zur Textdatei mit den Ranglistendaten.
         '''
         self.__file_data = []
 
@@ -32,25 +40,23 @@ class QuizRangliste():
             # file exists - open for reading
             with open(datei, 'r', encoding='utf-8') as file:
                 self.__extract_file_data(file)
-        except:
-            # file not exists - create it (w+ wipes file so I need 2 exceptions...)
+        except Exception:
+            # file not exists - create it
             try:
                 with open(datei, 'w+', encoding='utf-8') as file:
                     self.__extract_file_data(file)
-            except:
+            except Exception:
                 print("Fehlende Berechtigung auf dem Dateisystem!")
-
-        print(self.__file_data);
 
     def als_dictionary(self):
         '''
-        Gibt die iDaten als Dictionary von Dictionaries zurück:
+        Gibt die Daten als Dictionary von Dictionaries zurück:
             Key = name, Value = {’Punkte’: punkte, ’Zeit’: zeit}
 
         Die Datentypen der Elemente sind:
-            type(name) == str,
-            type(punkte) == int,
-            type(zeit) == float
+            name: string,
+            punkte: int,
+            zeit: float
         '''
         # ...
         pass
@@ -61,9 +67,9 @@ class QuizRangliste():
             Key = name, Value = {’Punkte’: punkte, ’Zeit’: zeit}
 
         Die Datentypen der Elemente sind:
-            type(name) == str,
-            type(punkte) == int,
-            type(zeit) == float
+            name: string,
+            punkte: int,
+            zeit: float
 
         Die Liste ist nach den folgenden Prioritäten sortiert:
             1. Punkte, absteigend
@@ -80,10 +86,12 @@ class QuizRangliste():
         Das Zeilen-Format ist:
             Name | Punkte | Zeit
 
-            wobei Punkte == Ganzzahl und Zeit == Float (auf 1 Kommastelle gerundet)
+            wobei Punkte == Ganzzahl und
+            Zeit == Float (auf 1 Kommastelle gerundet)
 
             Die Spaltenbreite ist für alle Zeilen gleich und wird
-            dynamisch anhand des längsten Elements in der jeweiligen Spalte ermittelt.
+            dynamisch anhand des längsten Elements
+            in der jeweiligen Spalte ermittelt.
 
             Die Ausrichtungen sind:
                 Name=linksbündig, Punkte=rechtsbündig, Zeit=rechtsbündig.
@@ -105,37 +113,38 @@ class QuizRangliste():
         Falls name nicht existiert: neuen Namen anlegen.
 
         Argumente:
-            type(name) == str, (darf keine Kommazeichen enthalten)
-            type(punkte) == int oder str,
-            type(zeit) == float oder str
+            name: string, (darf keine Kommazeichen enthalten)
+            punkte: int oder string,
+            zeit: float oder string
 
         Rückgabewert:
-            False bei ungültigen Argumenten,
-            True falls fehlerfrei.
+            False: Boolean - bei ungültigen Argumenten,
+            True: Boolean -  falls fehlerfrei.
         '''
         # ...
         pass
 
     def name_entfernen(self, name):
         '''
-        Löscht den die Person (name ink punkte und zeit) von der Liste.
-        Falls die angegebene Person (name) nicht in der Liste ist, passiert nichts.
-
-        Falls name bereits in den internen Daten existiert:
-            punkte und zeit hinzuaddieren.
-        Falls name nicht existiert: neuen Namen anlegen.
+        Löscht die Person (name, punkte und zeit) von der Liste.
+        Falls die angegebene Person (name) nicht in der Liste ist,
+        passiert nichts.
 
         Argumente:
-            type(name) == str
+            name: string
         '''
         # ...
         pass
 
     def speichern(self, als=None):
         '''
-        Schreibt die Daten als Kommagetrennte Werte in die ursprüngliche Textdatei.
-        Falls das Argument als angegeben wird:
-            schreibt die Daten in die angegebene Datei anstatt in die ursprüngliche Datei.
+        Schreibt die Daten als Kommagetrennte Werte (CSV)
+        in die ursprüngliche Textdatei.
+
+        Falls das Argument 'als' angegeben wird:
+            schreibt die Daten in die angegebene Datei
+            anstatt in die ursprüngliche Datei.
+
         Die Datei wird mit encoding=’utf-8’ geschrieben
 
         Die Liste ist nach den folgenden Prioritäten sortiert:
@@ -146,20 +155,24 @@ class QuizRangliste():
         # ...
         pass
 
+
 # --- Test --------------------------------------------------------------------
 if __name__ == '__main__':
     qr = QuizRangliste(datei='rangliste.txt')
 
     d = qr.als_dictionary()
     print(d)
-    #{’Noemi’: {’Punkte’: 7, ’Zeit’: 40.0}, ’Max’: {’Punkte’: 6, ’Zeit’: 100.02},
-    #’Hans’: {’Punkte’: 2, ’Zeit’: 45.24}, ’Anna’: {’Punkte’: 5, ’Zeit’: 20.55},
-    #’Fritz’: {’Punkte’: 5, ’Zeit’: 39.3}, ’Laura’: {’Punkte’: 5, ’Zeit’: 20.55}}
+    # {’Noemi’: {’Punkte’: 7, ’Zeit’: 40.0},
+    # ’Max’: {’Punkte’: 6, ’Zeit’: 100.02},
+    # ’Hans’: {’Punkte’: 2, ’Zeit’: 45.24},
+    # ’Anna’: {’Punkte’: 5, ’Zeit’: 20.55},
+    # ’Fritz’: {’Punkte’: 5, ’Zeit’: 39.3},
+    # ’Laura’: {’Punkte’: 5, ’Zeit’: 20.55}}
 
     lst = qr.als_liste()
     print(lst)
-    #[(’Noemi’, 7, 40.0), (’Max’, 6, 100.02), (’Anna’, 5, 20.55),
-    #(’Laura’, 5, 20.55), (’Fritz’, 5, 39.3), (’Hans’, 2, 45.24)]
+    # [(’Noemi’, 7, 40.0), (’Max’, 6, 100.02), (’Anna’, 5, 20.55),
+    # (’Laura’, 5, 20.55), (’Fritz’, 5, 39.3), (’Hans’, 2, 45.24)]
 
     s = qr.als_string()
     print(s)
